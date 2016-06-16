05e28e7947f2a55ce3a74b7ead6911c7ba30daed
Paul Bottein <paul.bottein@gmail.com>
2016-06-16 17:24:15 +0200

    Add items action functions (WIP)

--- chaincode.go
+++ chaincode.go
@@ -8,15 +8,14 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-// SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
 }
 
-var itemIndexStr = "_itemindex"                             //name for the key/value that will store a list of all known marbles
-var accountIndexStr = "_accountindex"	                            //name for the key/value that will store all open trades
+var itemIndexStr = "_itemindex"          //name for the key/value that will store all items
+var accountIndexStr = "_accountindex"	 //name for the key/value that will store all accounts
 
 type Item struct{
-	Name       string  `json:"name"`	                    //the fieldtags are needed to keep case from bouncing around
+	Name       string  `json:"name"`
 	Price      int     `json:"price"`
 }
 
@@ -25,9 +24,9 @@ type Account struct{
 	Money       int     `json:"money"`
 }
 
-// ============================================================================================================================
+// =============================================================================
 // Main
-// ============================================================================================================================
+// =============================================================================
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
@@ -35,9 +34,9 @@ func main() {
 	}
 }
 
-// ============================================================================================================================
+// =============================================================================
 // Init
-// ============================================================================================================================
+// =============================================================================
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
@@ -46,21 +45,27 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	return nil, nil
 }
 
-// ============================================================================================================================
+// =============================================================================
 // Invoke
-// ============================================================================================================================
+// =============================================================================
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running: " + function)
 
 	// Handle different functions
-	if function == "init" {													//initialize the chaincode state, used as reset
+	if function == "init" {
 		return t.Init(stub, "init", args)
 	} else if function == "createAccount" {
 		return t.createAccount(stub, args)
+	} else if function == "createItem" {
+		return t.createItem(stub, args)
+	} else if function == "sellItem" {
+		return t.sellItem(stub, args)
+	} else if function == "buyItem" {
+		return t.buyItem(stub, args)
 	}
-	fmt.Println("invoke did not find func: " + function)					//error
 
-	return nil, errors.New("Received unknown invocation function")
+	fmt.Println("Received unknown invoke function: " + function)
+	return nil, errors.New("Received unknown invoke function" + function)
 }
 
 func (t *SimpleChaincode) createAccount(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
@@ -92,32 +97,46 @@ func (t *SimpleChaincode) createAccount(stub *shim.ChaincodeStub, args []string)
 	err = append_id(stub, accountIndexStr, account.Username)
 	if err != nil {
 		fmt.Println("Error appending new username.")
-		return nil, errors.New("Error appending new username")
+		return nil, errors.New("Error appending new username.")
 	}
 
 	accountBytes, err := json.Marshal(account)
 	err = stub.PutState(account.Username, accountBytes)
 	if err != nil {
 		fmt.Println("Error putting account on ledger.")
-		return nil, errors.New("Error putting account on ledger")
+		return nil, errors.New("Error putting account on ledger.")
 	}
 
 	return nil, nil
 }
 
-// ============================================================================================================================
+func (t *SimpleChaincode) createItem(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+
+	return nil, nil
+}
+
+func (t *SimpleChaincode) sellItem(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+
+	return nil, nil
+}
+
+func (t *SimpleChaincode) buyItem(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+
+	return nil, nil
+}
+
+// =============================================================================
 // Query
-// ============================================================================================================================
+// =============================================================================
 func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running: " + function)
 
 	// Handle different functions
-	if function == "getAccount" {											//read a variable					//error
+	if function == "getAccount" {
 		return t.getAccount(stub, args)
 	}
-	fmt.Println("query did not find func: " + function)						//error
-
-	return nil, errors.New("Received unknown query function")
+	fmt.Println("Received unknown query function: " + function)
+	return nil, errors.New("Received unknown query function" + function)
 }
 
 func (t *SimpleChaincode) getAccount(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
@@ -146,9 +165,9 @@ func (t *SimpleChaincode) getAccount(stub *shim.ChaincodeStub, args []string) ([
 	return accountBytes, nil
 }
 
-// ============================================================================================================================
+// =============================================================================
 // Utils
-// ============================================================================================================================
+// =============================================================================
 func append_id(stub *shim.ChaincodeStub, indexStr string, id string) (error) {
 
 	// Retrieve existing index
@@ -169,12 +188,14 @@ func append_id(stub *shim.ChaincodeStub, indexStr string, id string) (error) {
 	// Marshal the index
 	jsonAsBytes, err := json.Marshal(tmpIndex)
 	if err != nil {
+		fmt.Println("Error storing new " + indexStr + " into ledger")
 		return errors.New("Error storing new " + indexStr + " into ledger")
 	}
 
 	// Store the index into the ledger
 	err = stub.PutState(indexStr, jsonAsBytes)
 	if err != nil {
+		fmt.Println("Error storing new " + indexStr + " into ledger")
 		return errors.New("Error storing new " + indexStr + " into ledger")
 	}
 
